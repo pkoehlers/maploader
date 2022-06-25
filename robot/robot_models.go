e832697ca226d0907946dcdae7d04e748dfd2426
pkoehlers <50806567+pkoehlers@users.noreply.github.com>
2022-06-25 13:15:18 +0200

    feat: add support for Dreame F9

--- robot/robot_models.go
+++ robot/robot_models.go
@@ -5,4 +5,8 @@ var robots []Robot = []Robot{
 		MapFolders:       []string{"/data/ri", "/data/map", "/data/DivideMap"},
 		mapFiles:         []string{"/data/config/ava/mult_map.json"},
 		restartProcesses: []Process{MiioClientProcess, AvaProcess}},
+	{model: "p2008", // Dreame F9
+		MapFolders:       []string{"/data/log/ri/", "/data/DivideMap"},
+		mapFiles:         []string{"/data/config/ava/mult_map.json", "/data/log/map_info.bin", "/data/log/slam.db"},
+		restartProcesses: []Process{MiioClientProcess, AvaProcess}},
 }
