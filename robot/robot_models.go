f5e6b000cff60fcbaf996d3737ab87a5e64d9aa4
Michał Wieluński <michal@wielunski.net>
2023-08-13 17:20:21 +0200

    dreame_d9_p2009

--- robot/robot_models.go
+++ robot/robot_models.go
@@ -9,6 +9,10 @@ var robots []Robot = []Robot{
 		MapFolders:       []string{"/data/log/ri/", "/data/DivideMap"},
 		mapFiles:         []string{"/data/config/ava/mult_map.json", "/data/log/map_info.bin", "/data/log/slam.db"},
 		restartProcesses: []Process{MiioClientProcess, AvaProcess}},
+	{model: "p2009", // Dreame D9
+		MapFolders:       []string{"/data/ri", "/data/map", "/data/DivideMap", "/data/DivideDebug"},
+		mapFiles:         []string{"/data/config/ava/mult_map.json", "/data/log/map_info.bin"},
+		restartProcesses: []Process{MiioClientProcess, AvaProcess}},
 	{model: "p2187", // Dreame D9 Pro
 		MapFolders:       []string{"/data/ri", "/data/map", "/data/DivideMap", "/data/DivideDebug"},
 		mapFiles:         []string{"/data/config/ava/mult_map.json", "/data/log/map_info.bin"},
